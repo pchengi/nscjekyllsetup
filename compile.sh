065a3746d2a6d78ba47a03762ed123c97e12f816
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-03-16 16:24:57 +0100

    update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-export LANG=en_US.UTF-8
-src=$1
-if [ "$src" = "nsc" ]; then
-        source /home/nscuser/setupnscruby;
-else
-        source /home/nscuser/setupruby;
-fi
+source /home/nscuser/singenv nsc
 cd /home/nscuser/mnt;
 jekyll build;
 exit $?;
