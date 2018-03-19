8af980eec90223941361bed7429ac56ffd7980d7
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-03-19 17:41:10 +0100

    fix path

--- presetup.sh
+++ presetup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Some prerequisites
-
 echo "en_US.UTF-8 UTF-8" >/etc/locale.gen
 locale-gen
 echo 'export LANG=en_US.UTF-8' >>/etc/bash.bashrc
-export 'RBENV_ROOT=/usr/local/src/rbenv
+export 'RBENV_ROOT'=/usr/local/src/rbenv
 echo "export PATH=$RBENV_ROOT/bin:$PATH" >>/etc/bash.bashrc
 ln -s /usr/bin/python2.7 /usr/bin/python
